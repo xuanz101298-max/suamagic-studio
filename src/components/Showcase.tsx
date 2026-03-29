6a1b7e08251200981ca4a6fda2c9dbde566ac37f
SuaMagic <xuanz101298@gmail.com>
2026-03-30 01:51:32 +0800

    fix: 移除自动播放，完全手动控制

--- src/components/Showcase.tsx
+++ src/components/Showcase.tsx
@@ -271,7 +271,7 @@ export default function Showcase({ works, setWorks, isEditMode }: ShowcaseProps)
                 <video
                   src={activeWork.mediaUrl}
                   controls
-                  autoPlay={!isMobile}
+                  autoPlay={false}
                   playsInline
                   webkit-playsInline
                   className="w-full max-h-[75vh] object-contain bg-[#111]"
